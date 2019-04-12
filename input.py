2adce6d8a511baefe03e058f0659606733ea1c78
ASharma <31654631+akshatsharma1205@users.noreply.github.com>
2019-04-12 13:41:32 +0530

    Add files via upload
    Required Input for the MACA Protocol.

--- input.py
+++ input.py
@@ -12,12 +12,12 @@ def show_entry_fields():
    rcvr = e2.get().upper()
 
    if [sndr, rcvr] in nghbr or [rcvr, sndr] in nghbr:
-   	print("Match Exist")
+   	print("Welcome, this is an implementation of MACA Protocol. \n")
    	maca.__main__(sndr, rcvr)
    	#subprocess.Popen("python3 /Users/computer/Desktop/WSN_Proj/maca.py")
    	#os.system
    else:
-   	print("no Match")
+   	print("The sender & receiver are not neighbours or no such nodes exist, please check the above configuration.")
 
 master = Tk()
 Label(master, text="Sender").grid(row=0)
@@ -28,6 +28,10 @@ e2 = Entry(master)
 
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
+
+print("The configuration of nodes are:")
+print("A :: B :: C :: D")
+print("\nWaiting for your input...")
 
 Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
 Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
