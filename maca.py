9e0a2ddc0c7c6dd7ecdfbe29cc2669c5fba28b40
ASharma <31654631+akshatsharma1205@users.noreply.github.com>
2019-04-12 13:40:55 +0530

    MACA Protocol Implementation

--- maca.py
+++ maca.py
@@ -14,17 +14,161 @@ b = Node("A", "C")
 c = Node("B", "D")
 d = Node("C", None)
 
+a_cord = [-300, 200]
+b_cord = [-150, 200]
+c_cord = [0, 200]
+d_cord = [150, 200]
+
+def maca(sender, receiver):
+  print("\nLets Begin...")
+
+  #Defined all the turtles with a lil bit description
+  draw1 = turtle.Turtle()
+  draw2 = turtle.Turtle()
+  draw3 = turtle.Turtle()
+  draw1.speed(0)
+  draw1.color("white")
+  draw2.speed(0)
+  draw2.color("white")
+  draw3.speed(0)
+  draw3.color("white")
+
+  #Just checked who is the sender and got the co-ordinates & node properties.
+  if sender in "A":
+    final = a
+    cord = a_cord
+  elif sender in "B":
+    final = b
+    cord = b_cord
+  elif sender in "C":
+    final = c
+    cord = c_cord
+  else:
+    final = d
+    cord = d_cord
+
+  time.sleep(3)
+
+  #I'll hide all the turtle pointers.
+  draw2.ht()
+  draw1.ht()
+  draw3.ht()
+
+  if True:
+    #All the turtles arrive on the Sender Node
+    draw1.goto(cord)
+    draw2.goto(cord)
+    draw3.goto(cord)
+  
+    #Defined properties for the sender nodes.
+    #I'm considering draw 3 for the initial RTS packet to the non-receiver node.
+    #I'm considering draw 1 and draw 2 to lead the further RTS & CTS with the receiver node.
+    draw3.color("violet")
+    draw2.color("white")
+    draw1.color("red")
+    draw1.width(30)
+    draw1.speed(1)
+    draw3.width(30)
+
+    #Now, let's send the RTS packet to the reciver and non-receiver node.
+
+    #If the left or right is NONE, then draw3 should not act, thus making draw3 properties none
+    #if final.left and final.right is None:
+      #I'll set the draw 3 color the white so that it doesn't affect the code.
+      
+    
+    #Here, I'll set the turtle face, where they have to proceed.
+    # for that, we need to check if the receiver is in the left/right of the sender.
+    # face = True means in right.
+    if final.left is None:
+      face = True
+      draw3.color("white")
+      draw3.width(1)
+    elif final.right is None:
+      face = False
+      draw3.color("white")
+      draw3.width(1)
+    elif receiver in final.left:
+      face = False
+    else:
+      face = True
+
+    #Turning the faces of turtles.
+    if face:
+      draw2.left(315)
+      draw1.left(315)
+      draw3.left(225)
+    else:
+      draw2.left(225)
+      draw1.left(225)
+      draw3.left(315)
+
+    # To get the animation effect : )
+    print("Sender is sending the RTS (Request to Send) Packet ...")
+    for _ in range(210):
+      draw3.forward(1)
+      draw1.forward(1)
+      draw2.forward(1)
+
+
+    draw2.color("red")
+    time.sleep(2)
+
+    draw2.width(30)
+    draw2.speed(1)
+    
+    #checked
+    if face:
+      draw2.left(270)
+    else:
+      draw2.left(90)
+
+
+    draw1.color("violet")
+    print("Receiver is reverting with the CTS (Clear to Send) Packet ...")
+    for _ in range(210):
+      draw2.forward(1)
+      draw1.forward(1)
+    
+
+    time.sleep(1)
+    #Sending the Data Packet
+    print("Sender is sending the DATA packet..")
+    if face:
+      draw2.left(90)
+    else:
+      draw2.left(270)
+
+    draw2.color("green")
+    
+    draw2.width(60)
+    draw2.forward(210)
+
+    #Sending the Acknowledgement
+    print("Receiver is sending Acknowledgement...")
+    if face:
+      draw2.left(225)
+    else:
+      draw2.left(135)
+
+    draw2.width(10)
+    draw2.forward(150)
+
+    time.sleep(1)
+    print("This program illustrates the MACA (Multiple Access Collision Avoidance) Protocol. \nThanks for your time.")
+
+
+
+
+
 def __main__(sender, receiver):
+  print("Input Received!")
   wn = turtle.Screen()
   wn.title("MACA Protocol")
   wn.bgcolor("white")
   wn.setup(width=800, height=600)
-  wn.tracer(0)
+  #wn.tracer(0)
 
-  a_cord = [-300, 200]
-  b_cord = [-150, 200]
-  c_cord = [125, 200]
-  d_cord = [300, 200]
 
   # Node A
   node_a = turtle.Turtle()
@@ -33,11 +177,11 @@ def __main__(sender, receiver):
   node_a.color("black")
   node_a.shapesize(stretch_wid=2,stretch_len=2)
   node_a.penup()
-  node_a.goto(-300, 200)
+  node_a.goto(a_cord)
 
   line_a = turtle.Turtle()
   line_a.color("white")
-  line_a.goto(-300, 200)
+  line_a.goto(a_cord)
   line_a.color("black")
   line_a.left(270)
   line_a.forward(610)
@@ -59,11 +203,11 @@ def __main__(sender, receiver):
   node_b.color("red")
   node_b.shapesize(stretch_wid=2,stretch_len=2)
   node_b.penup()
-  node_b.goto(-150, 200)
+  node_b.goto(b_cord)
 
   line_b = turtle.Turtle()
   line_b.color("white")
-  line_b.goto(-150, 200)
+  line_b.goto(b_cord)
   line_b.color("red")
   line_b.left(270)
   line_b.forward(610)
@@ -79,17 +223,17 @@ def __main__(sender, receiver):
   node_c.color("blue")
   node_c.shapesize(stretch_wid=2,stretch_len=2)
   node_c.penup()
-  node_c.goto(125, 200)
+  node_c.goto(c_cord)
 
   line_c = turtle.Turtle()
   line_c.color("white")
-  line_c.goto(125, 200)
+  line_c.goto(c_cord)
   line_c.color("blue")
   line_c.left(270)
   line_c.forward(610)
   line_c.penup()
 
-  pen.goto(125, 220)
+  pen.goto(0, 220)
   pen.write("C")
 
   # Node D 
@@ -99,47 +243,20 @@ def __main__(sender, receiver):
   node_d.color("green")
   node_d.shapesize(stretch_wid=2,stretch_len=2)
   node_d.penup()
-  node_d.goto(300, 200)
+  node_d.goto(d_cord)
 
   line_d = turtle.Turtle()
   line_d.color("white")
-  line_d.goto(300, 200)
+  line_d.goto(d_cord)
   line_d.color("green")
   line_d.left(270)
   line_d.forward(610)
   line_d.penup()
 
-  pen.goto(300, 220)
+  pen.goto(150, 220)
   pen.write("D")
 
-  while True:
-    wn.update()
-
-    #time.sleep(10)
-    print("The sender is:" + sender)
-    print("\nThe receiver is:" + receiver)
-
-    #time.sleep(3)
-    draw1 = turtle.Turtle()
-    draw1.speed(1)
-    draw1.color("white")
-
-    if sender is "A":
-      draw1.goto(a_cord)
-    elif sender is "B":
-      draw1.goto(b_cord)
-    elif sender is "C":
-      draw1.goto(c_cord)
-    else:
-      draw1.goto(d_cord)
-
-    draw1.width(50)
-    draw1.forward(100)
-
-
-
-
-
-# #Main Loop
-# while True:
-#   wn.update()
+  print("\nThe sender is:" + sender)
+  print("The receiver is:" + receiver)
+  maca(sender, receiver)
+  
